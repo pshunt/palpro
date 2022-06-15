ca28739c372e434f3fc53bf50d3c1d0999dd240d
pshunt <peter.s.hunt@gmail.com>
2022-06-14 18:57:40 -0700

    add message create

--- palpro/palpro/base/views.py
+++ palpro/palpro/base/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from .models import Room, Topic
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # rooms = [
@@ -22,7 +23,7 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username') 
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
     
         try:
@@ -46,8 +47,19 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    page = 'register'
-    return render(request, 'base/login_register.html')
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+    return render(request, 'base/login_register.html', {'form':form})
 
 
 def home(request):
@@ -71,8 +83,20 @@ def room(request, pk):
     #     if i['id'] == int(pk):
     #         room = i
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+    
+    context = {'room': room, 'room_messages':room_messages}
     return render(request, 'base/room.html', context)
+
+
 
 @login_required(login_url='login')
 def createRoom(request):
